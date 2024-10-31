95c53510dfaa75475159939d13793dc2165863fe
spaciccio <cecconi2007@gmail.com>
2024-10-31 11:56:10 +0100

    Aggiunto main.py alla cartella public e aggiornato configurazioni

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\cecco\Desktop\vediao makear\myenv
+set VIRTUAL_ENV=C:\xampp\htdocs\vediao makear\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
